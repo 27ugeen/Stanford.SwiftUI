ef6538153e1154d883de48115ef727ab3da9bbab
27ugeen <27ugeen@gmail.com>
2023-10-17 13:39:17 +0300

    Inital Commit CardView isFaceUp Togglable isFaceUp

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by GiN Eugene on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
